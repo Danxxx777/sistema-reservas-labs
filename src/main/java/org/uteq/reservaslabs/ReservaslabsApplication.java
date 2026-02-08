b5769d9b176db878174c1b914ec4f13e23b48993
BYRONDANIELLOORMENDO <bloorm2@uteq.edu.ec>
2026-02-07 22:00:10 -0500

    Proyecto base:Spring Boot + JPA

---
+++ src/main/java/org/uteq/reservaslabs/ReservaslabsApplication.java
@@ -0,0 +1,13 @@
+package org.uteq.reservaslabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaslabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservaslabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/uteq/reservaslabs/ReservaslabsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uteq.reservaslabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaslabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
